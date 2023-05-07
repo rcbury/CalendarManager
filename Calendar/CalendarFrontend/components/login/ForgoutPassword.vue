679014ea7d9e50d2f002f293efdf56e2f8cb5c82
Konstantin <38701987+SteepMans@users.noreply.github.com>
2023-05-07 12:21:22 +0300

    [Frontend] Forgout password

---
+++ Calendar/CalendarFrontend/components/login/ForgoutPassword.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="application-login-main__authorization">
+        <v-form ref="form">
+            <v-text-field
+                v-model="loginField"
+                :counter="20"
+                :rules="baseRules"
+                label="Write the email to which the account was registered"
+                required
+            ></v-text-field>
+
+            <v-btn class="mt-4" color="success" block @click="validate">
+                reset password
+            </v-btn>    
+
+            <v-btn class="mt-4" @click="$emit('changeForm', 'login')">
+                Authorization
+            </v-btn>    
+        </v-form>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => ({
+      emailField: '',
+
+      baseRules: [
+        v => !!v || 'Is required',
+        v => (v && v.indexOf("@") >= 0) || 'Invalid email',
+      ]
+    }),
+
+    methods: {
+      async validate  () {
+        const valid = await this.$refs.form.validate()
+
+        if (valid) alert('send email')
+      },
+
+      reset () {
+        this.$refs.form.reset()
+      },
+    },
+}
+</script>
+
+<style>
+
+</style>
--- Calendar/CalendarFrontend/store/authorization/state.js
+++ Calendar/CalendarFrontend/store/authorization/state.js
@@ -1,3 +1,3 @@
 export default () => ({
-    isAuthorization: true
+    isAuthorization: false
 })
